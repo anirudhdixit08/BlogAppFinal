662b5e732628c3356f1ccd787b1a426cb2a7d0ce
Anirudh Dixit <anirudhdixit@Anirudhs-MacBook-Air.local>
2025-08-01 10:16:15 +0530

    Done

--- backend/index.js
+++ backend/index.js
@@ -22,9 +22,10 @@ app.use(cookieParser());
 
 // CORS configuration for deployment
 const allowedOrigins = [
-  'https://blog-app-final-git-main-anirudh-dixits-projects.vercel.app', // Make sure to use your correct frontend URLs
+  'https://blog-app-final-git-main-anirudh-dixits-projects.vercel.app',
   'https://blog-app-final-five.vercel.app',
-  'blog-app-final-ks0rik0vw-anirudh-dixits-projects.vercel.app'
+  'blog-app-final-ks0rik0vw-anirudh-dixits-projects.vercel.app',
+  'https://blog-app-final-jauaxyjiz-anirudh-dixits-projects.vercel.app' // <-- ADD THIS NEW URL
 ];
 
 app.use(
